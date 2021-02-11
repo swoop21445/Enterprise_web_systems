83677945ad3d3be123a4c8c18218dd83ab41760e
swoop21445 <davb.gordon@gmail.com>
2021-02-11 18:17:25 +0000

    First commit to test aws

---
+++ node-server-aws/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+const test_element = <h1>Testing if the aws server works</h1>
+ReactDOM.render(test_element, Document.getElementById("root"))
+
