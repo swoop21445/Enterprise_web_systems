fcd06add1a33ec76586cfabcb1ef05d3f60cfc60
swoop21445 <davb.gordon@gmail.com>
2021-03-04 19:10:57 +0000

    added tracks and placeholder images

--- node-server-aws/aws-react/src/App.js
+++ node-server-aws/aws-react/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import highoctaneimg from './files/highoctane.jpg'
+import highoctane from './files/bensound-highoctane.ogg'
+import allthatimg from './files/allthat.jpg'
+import all_that from './files/bensound-allthat.mp3'
 import './App.css';
+import ReactAudioPlayer from 'react-audio-player';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <header><h1>Pick the track you like best!</h1></header>
+      <div className='track_1'>
+        <img alt='track 1 high octane' src={highoctaneimg}></img>
+        <div className = 'track_footer'>
+          <button className='track_select'>Select this track</button>
+            <ReactAudioPlayer
+            src={highoctane}
+            controls
+            />
+        </div>
+      </div>
+      <div className = 'track_2'>
+      <img alt='track 1 high octane' src={allthatimg}></img>
+      <div className = 'track_footer'>
+        <button className='track_select'>Select this track</button>
+        <ReactAudioPlayer
+        src = {all_that}
+        controls
+        />
+      </div>
+      </div>
     </div>
   );
 }
