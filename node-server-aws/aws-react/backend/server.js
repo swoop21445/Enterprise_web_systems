d2ff42078b25a7dba906cf1b9a19f16b8f6e01ea
swoop21445 <davb.gordon@gmail.com>
2021-03-08 14:39:36 +0000

    third party additions

--- node-server-aws/aws-react/backend/server.js
+++ node-server-aws/aws-react/backend/server.js
@@ -12,8 +12,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true});
 
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("mongoDB database connection established")
+})
 
 app.listen(port, () => {
     console.log(`server active on: ${port}`);
--- node-server-aws/aws-react/src/App.js
+++ node-server-aws/aws-react/src/App.js
@@ -1,5 +1,5 @@
 import highoctaneimg from './files/highoctane.jpg'
-import highoctane from './files/bensound-highoctane.ogg'
+import high_octane from './files/bensound-highoctane.ogg'
 import allthatimg from './files/allthat.jpg'
 import all_that from './files/bensound-allthat.mp3'
 import './App.css';
@@ -10,25 +10,27 @@ function App() {
   return (
     <div className='App'>
       <header><h1>Pick the track you like best!</h1></header>
-      <div className='track_1'>
-        <img alt='track 1 high octane' src={highoctaneimg}></img>
+      <div className ='container'>
+        <div className='track-item'>
+          <img alt='track 1 high octane' src={highoctaneimg}></img>
+          <div className = 'track_footer'>
+            <button className='track_select'>Select this track</button>
+              <ReactAudioPlayer
+              src={high_octane}
+              controls
+              />
+          </div>
+        </div>
+        <div className = 'track-item'>
+        <img alt='track 2 all that' src={allthatimg}></img>
         <div className = 'track_footer'>
           <button className='track_select'>Select this track</button>
-            <ReactAudioPlayer
-            src={highoctane}
-            controls
-            />
+          <ReactAudioPlayer
+          src = {all_that}
+          controls
+          />
         </div>
-      </div>
-      <div className = 'track_2'>
-      <img alt='track 1 high octane' src={allthatimg}></img>
-      <div className = 'track_footer'>
-        <button className='track_select'>Select this track</button>
-        <ReactAudioPlayer
-        src = {all_that}
-        controls
-        />
-      </div>
+        </div>
       </div>
     </div>
   );
